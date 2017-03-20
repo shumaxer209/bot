e2b2edbc13490ab4b633f7fd397e4f14761e1c18
shumaxer209 <shumaxer1353@mail.ru>
2017-03-20 13:40:57 +0300

    correct2

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+ 
+var app = express();
+
+var jsonParser = bodyParser.json();
+ 
+app.use(express.static(__dirname + "/public"));
+ 
+app.post("/user", jsonParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.json(`${request.body.userName} - ${request.body.userPass}`);
+});
+ 
+app.get("/", function(request, response){
+     
+    response.send();
+});
+ 
+app.listen(3000);
